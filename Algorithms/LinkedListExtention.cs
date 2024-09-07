d40e8ae34a207e04afef6a32f0635b40beea86eb
Azat Kashapov <ichiru98@mail.ru>
2024-09-07 13:40:48 +0300

    added SumOfTwoLists

---
+++ Algorithms/LinkedListExtention.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsDataStructures
+{
+    public static class LinkedListExtention
+    {
+        public static List<int> SumOfTwoLists(this LinkedList _firstList, LinkedList _secondList)
+        {
+            List<int> result = new List<int>();
+
+            if (_firstList.head == null || _secondList.head == null)
+                return result;
+
+            if (_firstList.Count() != _secondList.Count())
+                return result;
+
+            Node firstListPointer = _firstList.head;
+            Node secondListPointer = _secondList.head;
+
+            while (firstListPointer != null && secondListPointer != null)
+            {
+                var sum = firstListPointer.value + secondListPointer.value;
+                result.Add(sum);
+                firstListPointer = firstListPointer.next;
+                secondListPointer = secondListPointer.next;
+            }
+            return result;
+        }
+    }
+}
--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -14,14 +14,11 @@ namespace AlgorithmsDataStructures
             var thiedNode = new Node(3);
             var ninethNode = new Node(9);
             var ninethNode2 = new Node(9);
-
-            var testNode2 = new Node(12);
-
+            
             list.AddInTail(firstNode);
             list.AddInTail(ninethNode);
             list.AddInTail(ninethNode2);
             list.AddInTail(secondNode);
-
 
             var secondList = new LinkedList();
             var fourthNode = new Node(4);
@@ -31,32 +28,8 @@ namespace AlgorithmsDataStructures
             secondList.AddInTail(fourthNode);
             secondList.AddInTail(sixthNode);
             secondList.AddInTail(seventhNode);
+
             Console.ReadLine();
-        }
-
-
-        public static List<int> SumOfTwoLists(LinkedList _firstList, LinkedList _secondList)
-        {
-            List<int> result = new List<int>();
-
-            if (_firstList.head == null || _secondList.head == null)
-                return result;
-
-
-            if (_firstList.Count() != _secondList.Count())
-                return result;
-
-            Node firstListPointer = _firstList.head;            
-            Node secondListPointer = _secondList.head;
- 
-            while(firstListPointer != null && secondListPointer != null)
-            {
-                var sum = firstListPointer.value + secondListPointer.value;
-                result.Add(sum);
-                firstListPointer = firstListPointer.next;
-                secondListPointer = secondListPointer.next;
-            }
-            return result;
         }
     }
 }
