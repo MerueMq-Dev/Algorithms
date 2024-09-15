5549cadcea57a775fa8aa16e1b2a46638db9a64a
LAPTOP\work <ichiru98@mail.ru>
2024-09-15 14:26:22 +0300

    dynamic array

---
+++ Algorithms/DynArray.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsDataStructures
+{
+    public class DynArray<T>
+    {
+        public T[] array;
+        public int count;
+        public int capacity;
+
+        private const int GrowthFactor = 2;
+        private const double ShrinkFactor = 1.5;
+        
+        public DynArray()
+        {
+            count = 0;
+            MakeArray(16);
+        }
+
+        public void MakeArray(int new_capacity)
+        {
+            capacity = new_capacity < 16 ? 16 : new_capacity;
+
+            if (array == null)
+                array = new T[capacity];
+                    
+            var newArr = new T[capacity];
+            Array.Copy(array, newArr, count);
+            array = newArr;
+        }
+
+        public T GetItem(int index)
+        {
+            ValidateIndex(index);
+            return array[index];
+        }
+
+        public void Append(T itm)
+        {
+            if (count == 0)
+            {
+                array[0] = itm;
+                count++;
+                return;
+            }
+
+            if (count == capacity)
+                MakeArray(capacity * GrowthFactor);
+
+            array[count] = itm;
+            count++;
+        }
+
+        public void Insert(T itm, int index)
+        {
+            ValidateIndex(index, true);
+
+            if (count == capacity)
+                MakeArray(capacity * GrowthFactor);
+
+            for (int i = count; i > index; i--) 
+                array[i] = array[i - 1];
+
+            array[index] = itm;
+            count++;
+        }
+
+        public void Remove(int index)
+        {
+            ValidateIndex(index);
+            
+            for (int i = index; i < count; i++) 
+                array[i] = array[i + 1];
+            count--;
+            
+            if (count >= capacity / 2)
+                return;
+            
+            var newCapacity = (int)(capacity / ShrinkFactor);
+            MakeArray(newCapacity);
+        }
+
+        private void ValidateIndex(int targetIndex, bool includeLastItem = false)
+        {
+            var itemsCount = includeLastItem ? count : count - 1;
+            
+            if (targetIndex > itemsCount || targetIndex < 0)
+            {
+                throw new IndexOutOfRangeException();
+            }
+        }
+    }
+}
--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -8,58 +8,58 @@ namespace AlgorithmsDataStructures
     {
         static void Main(string[] args)
         {
-            var list = new LinkedList2();
-            var node1 = new Node(1);
-            // var node2 = new Node(2);
-            var node11 = new Node(11);
-            var node3 = new Node(1);
-            var node4 = new Node(1);
-            var node12 = new Node(12);
-            // var node5 = new Node(5);
-            var node13 = new Node(13);
-            
-            list.AddInTail(node1);
-            // list.AddInTail(node2);
-            list.AddInTail(node11);
-            list.AddInTail(node3);
-            list.AddInTail(node12);
-            list.AddInTail(node4);
-            // list.AddInTail(node5);
-            list.AddInTail(node13);
-            
-            var currentNode = list.head;
-            Console.WriteLine("BEFORE");
-            if(currentNode == null) 
-                Console.WriteLine("List is empty");
-            while (currentNode != null)
-            {
-                Console.WriteLine($"----------------------------------------------------------------------");
-                Console.WriteLine($"previous  value: {currentNode?.prev?.value}");
-                Console.WriteLine($"current value: {currentNode?.value}");
-                Console.WriteLine($"next value: {currentNode?.next?.value}");
-                Console.WriteLine($"----------------------------------------------------------------------");
-                currentNode = currentNode.next;
-            }
-
-            var elementsCount =  list.Count();
-            Console.WriteLine($"elementsCount {elementsCount}");
-
-            var newNode = new Node(22);
-            list.RemoveAll(13);
-            
-            Console.WriteLine("AFTER");
-            currentNode = list.head;
-            if(currentNode == null) 
-                Console.WriteLine("List is Empty");
-            while (currentNode != null)
-            {
-                Console.WriteLine($"----------------------------------------------------------------------");
-                Console.WriteLine($"previous  value: {currentNode?.prev?.value}");
-                Console.WriteLine($"current value: {currentNode?.value}");
-                Console.WriteLine($"next value: {currentNode?.next?.value}");
-                Console.WriteLine($"----------------------------------------------------------------------");
-                currentNode = currentNode.next;
-            }
+            // var list = new LinkedList2();
+            // var node1 = new Node(1);
+            // // var node2 = new Node(2);
+            // var node11 = new Node(11);
+            // var node3 = new Node(1);
+            // var node4 = new Node(1);
+            // var node12 = new Node(12);
+            // // var node5 = new Node(5);
+            // var node13 = new Node(13);
+            //
+            // list.AddInTail(node1);
+            // // list.AddInTail(node2);
+            // list.AddInTail(node11);
+            // list.AddInTail(node3);
+            // list.AddInTail(node12);
+            // list.AddInTail(node4);
+            // // list.AddInTail(node5);
+            // list.AddInTail(node13);
+            //
+            // var currentNode = list.head;
+            // Console.WriteLine("BEFORE");
+            // if(currentNode == null) 
+            //     Console.WriteLine("List is empty");
+            // while (currentNode != null)
+            // {
+            //     Console.WriteLine($"----------------------------------------------------------------------");
+            //     Console.WriteLine($"previous  value: {currentNode?.prev?.value}");
+            //     Console.WriteLine($"current value: {currentNode?.value}");
+            //     Console.WriteLine($"next value: {currentNode?.next?.value}");
+            //     Console.WriteLine($"----------------------------------------------------------------------");
+            //     currentNode = currentNode.next;
+            // }
+            //
+            // var elementsCount =  list.Count();
+            // Console.WriteLine($"elementsCount {elementsCount}");
+            //
+            // var newNode = new Node(22);
+            // list.RemoveAll(13);
+            //
+            // Console.WriteLine("AFTER");
+            // currentNode = list.head;
+            // if(currentNode == null) 
+            //     Console.WriteLine("List is Empty");
+            // while (currentNode != null)
+            // {
+            //     Console.WriteLine($"----------------------------------------------------------------------");
+            //     Console.WriteLine($"previous  value: {currentNode?.prev?.value}");
+            //     Console.WriteLine($"current value: {currentNode?.value}");
+            //     Console.WriteLine($"next value: {currentNode?.next?.value}");
+            //     Console.WriteLine($"----------------------------------------------------------------------");
+            //     currentNode = currentNode.next;
+            // }
             
             // var secondList = new LinkedList2();
             // var fourthNode = new Node(4);
@@ -70,7 +70,31 @@ namespace AlgorithmsDataStructures
             // secondList.AddInTail(sixthNode);
             // secondList.AddInTail(seventhNode);
 
-            Console.ReadLine();
+            var dynArr = new DynArray<int>();
+            for (int i = 1; i <= 16; i++)
+            {
+                dynArr.Append(i);
+            }
+
+            Console.WriteLine("BEFORE: ");
+            Console.WriteLine($"Count: {dynArr.count}");
+            Console.WriteLine($"Capacity: {dynArr.capacity}");
+            for (int i = 0; i < dynArr.count; i++)
+            {
+                Console.WriteLine($"Dynamic arr element: {dynArr.GetItem(i)}");
+            }
+            
+            dynArr.Insert(550,0);
+            dynArr.Remove(4);
+            dynArr.Remove(100);
+            Console.WriteLine("AFTER: ");
+            Console.WriteLine($"Count: {dynArr.count}");
+            Console.WriteLine($"Capacity: {dynArr.capacity}");
+            for (int i = 0; i < dynArr.count; i++)
+            {
+                Console.WriteLine($"Dynamic arr element: {dynArr.GetItem(i)}");
+            }
+            // Console.ReadLine();
         }
     }
 }
