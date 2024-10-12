5df3f93cbb9769df1191fcc963e2481d762e16fa
LAPTOP\work <ichiru98@mail.ru>
2024-10-12 20:18:28 +0300

    no message

--- Algorithms/PowerSet.cs
+++ Algorithms/PowerSet.cs
@@ -53,13 +53,13 @@ namespace AlgorithmsDataStructures
                 }
             }
 
-            for (var startIndex = 0; startIndex != slotIndex; startIndex++)
-            {
-                if (slots[startIndex] != null && slots[startIndex] == value)
-                {
-                    return startIndex;
-                }
-            }
+            // for (var startIndex = 0; startIndex != slotIndex; startIndex++)
+            // {
+            //     if (slots[startIndex] != null && slots[startIndex] == value)
+            //     {
+            //         return startIndex;
+            //     }
+            // }
 
             return -1;
         }
@@ -79,14 +79,14 @@ namespace AlgorithmsDataStructures
                     return startIndex;
                 }
             }
-
-            for (var startIndex = 0; startIndex != slotIndex; startIndex++)
-            {
-                if (slots[startIndex] == null)
-                {
-                    return startIndex;
-                }
-            }
+            //
+            // for (var startIndex = 0; startIndex != slotIndex; startIndex++)
+            // {
+            //     if (slots[startIndex] == null)
+            //     {
+            //         return startIndex;
+            //     }
+            // }
             
             return -1;
         }
